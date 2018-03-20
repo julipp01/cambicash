0b213d38ffc54c569654cd4af55ab3394e957e75
mendozavanessa <144995@unsaac.edu.pe>
2018-03-20 14:55:55 -0500

    arreglando vista mobile

--- src/components/viewOne/DataUser.js
+++ src/components/viewOne/DataUser.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const DataUser = ({dataUser}) => (
-  <div className="col-md-4">
+  <div className="col-md-4 d-flex justify-content-center margin-bottom">
     <div className="card" style={{width: '18rem'}}>
       <h2 className="text-center">{dataUser.title}</h2>
-      <img className="card-img-top" src={dataUser.url} alt="Card image cap" />
+      <img className="card-img-top rounded-circle" src={dataUser.url} alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title text-center">{dataUser.name}</h5>
         <p className="card-text text-center">DNI : {dataUser.dni}</p>
