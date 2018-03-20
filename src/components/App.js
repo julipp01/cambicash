a85cf4bbfc34decd1bf2e6544b4e3c339385188f
AraceliCumen <aycuevam@gmail.com>
2018-03-20 15:09:51 -0500

    subiendo componente Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import Header from './Header/Header'
 import Main from './viewOne/Main';
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <Main />
     </div>
   );
