b6b8dc57ac1ad2b350a85761a2257c070c78d0cb
mendozavanessa <144995@unsaac.edu.pe>
2018-03-20 06:22:17 -0500

    modificando nombres de botones

--- src/components/viewOne/List.js
+++ src/components/viewOne/List.js
@@ -7,10 +7,10 @@ const List = () => (
       <div className="col-md-12 fixed">
             <br />
             <br />
-            <button type="button" className="pading btn btn-outline-success">Success</button>
-            <button type="button" className="pading btn btn-outline-primary">Success</button>
-            <button type="button" className="pading btn btn-outline-success">Success</button>
-            <button type="button" className="pading btn btn-outline-primary">Success</button>
+            <button type="button" className="pading btn btn-outline-success">Cuentas Bancarias Personales</button>
+            <button type="button" className="pading btn btn-outline-primary">Cuentas Bancarias de Terceros</button>
+            <button type="button" className="pading btn btn-outline-success">Operaciones Historicas</button>
+            <button type="button" className="pading btn btn-outline-primary">Cuentas Bacrias de Rextie</button>
       </div>
     </div>
   </div>
