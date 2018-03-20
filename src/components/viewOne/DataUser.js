85f3b38f9fc683f572faf90ee935a8154cdb93fa
mendozavanessa <144995@unsaac.edu.pe>
2018-03-20 06:35:57 -0500

    creando data de usuario

--- src/components/viewOne/DataUser.js
+++ src/components/viewOne/DataUser.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DataUser = () => (
+const DataUser = ({dataUser}) => (
   <div className="col-md-4">
     <div className="card" style={{width: '18rem'}}>
-      <h2 className="text-center">Cuenta Personal</h2>
-      <img className="card-img-top" src="http://www.metalcivasquez.com.bo/php/images/contact-women.jpg" alt="Card image cap" />
+      <h2 className="text-center">{dataUser.title}</h2>
+      <img className="card-img-top" src={dataUser.url} alt="Card image cap" />
       <div className="card-body">
-        <h5 className="card-title text-center">Mery Luz Umiyauri Mendoza</h5>
-        <p className="card-text text-center">DNI : 70234567</p>
+        <h5 className="card-title text-center">{dataUser.name}</h5>
+        <p className="card-text text-center">DNI : {dataUser.dni}</p>
       </div>
     </div>
   </div>
--- src/components/viewOne/MainPrimary.js
+++ src/components/viewOne/MainPrimary.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import DataUser from './DataUser';
 import Welcome from './Welcome';
 
+const data = {
+  title: 'Cuenta Personal',
+  url: 'http://www.metalcivasquez.com.bo/php/images/contact-women.jpg',
+  name: 'Mery Luz Umiyauri Mendoza',
+  dni: '70234567',
+}
+
 const MainPrimary = () => (
   <div>
     <div className="container">
       <br />
       <div className="row">
-        <DataUser />
+        <DataUser dataUser = {data}/>
         <Welcome />
       </div>
     </div>
