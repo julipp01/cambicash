2e12293f96ee27852a7423a33d0b7c2cbc48b9c3
AraceliCumen <aycuevam@gmail.com>
2018-03-31 09:24:09 -0500

    actualizando estilos

--- src/components/Operation.js
+++ src/components/Operation.js
@@ -8,44 +8,7 @@ const Operation = () => (
           <div className="text-center">
             <p className="title-c-1 mb-0 padding-top-10">Cambio de dólares online</p>
             <p className="title-c-2 mt-0">Seguro, rápido y al mejor precio</p>
-          </div>
-          <div className="row text-center w-box m-auto" >
-            <div className={"col-6 box-green-1"}>
-              <p className="mb-0 p-2">Compra: 3.2480 </p>
-            </div>
-            <div className={"col-6 box-green-2 "}>
-              <p className="mb-0 p-2">Venta: 3.2700</p>
-            </div>
-          </div>
-          <div className={"container mt-3"}>
-            <div className="row" >
-              <div className="col-6 border ">
-                <label className="grey-2 f14 mb-0" htmlFor="">Tu envías:</label>
-                <input className="input-c" type="number" maxLength="10" />
-              </div>
-              <div className="col-6 border pt-2">
-                <div className=" d-inline-block">
-                  <img className="flag" src="images/peru.png" alt="eeuu" />
-                </div>
-                <div className="d-inline-block">
-                  <p className=" money">PEN S/</p>
-                </div>
-              </div>
-            </div>
-            <div className="row" >
-              <div className="col-6 border">
-                <label className="grey-2 f14" htmlFor="">Tú Recibes:</label>
-                <input type="text" className="input-c" maxLength="20" />
-              </div>
-              <div className="col-6 border pt-2">
-                <div className=" d-inline-block">
-                  <img className="flag" src="images/united-states.png" alt="eeuu" />
-                </div>
-                <div className=" d-inline-block">
-                  <p className=" money">USD $</p>
-                </div>
-              </div>
-            </div>
+            <p className="title-c-3 mt-0">Compra: 3.2480 - Venta: 3.2700</p>
           </div>
           <div className="text-center grey-2 mt-3" >
             <p className="title-4 mb-0 ">Ahorra S/. 100.00 respecto al banco</p>
--- src/components/OperationMenu.js
+++ src/components/OperationMenu.js
@@ -5,7 +5,7 @@ const OperationMenu = () => (
     <div className="process-row nav nav-tabs">
       <div className="process-step">
         <button type="button" className="btn btn-info btn-circle" data-toggle="tab" href="#menu1"><i className="fa fa-info fa-3x" /></button>
-        <p className="text-color-white"><small>Nueva <br />Operación</small></p>
+        <p className="text-color-white"><small>Tu <br />Operación</small></p>
       </div>
       <div className="process-step">
         <button type="button" className="btn btn-default btn-circle" data-toggle="tab" href="#menu2"><i className="fa fa-file-text-o fa-3x" /></button>
