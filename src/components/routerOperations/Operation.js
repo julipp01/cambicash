ae04de34a6cc79cb98705f924630cbe1238de3b5
julipp01 <julio_arpp@hotmail.com>
2025-01-08 19:48:57 -0500

    Primer commit del proyecto

--- src/components/routerOperations/Operation.js
+++ src/components/routerOperations/Operation.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import Converter from '../../containers/change-container';
-import Confirmation from '../../containers/confirmation-container'
-import Notification from '../../containers/notification-container'
+import Confirmation from '../../containers/confirmation-container';
+import Notification from '../../containers/notification-container';
 
 const Operation = () => (
-  <div className=" col-lg-6">
-    <Switch>
-      <Route exact path="/" component={Converter} />
-      <Route path="/confirmation" component={Confirmation} />
-      <Route path="/notification" component={Notification} />
-    </Switch>
+  <div className="col-lg-6">
+    <Routes>
+      <Route path="/" element={<Converter />} />
+      <Route path="/confirmation" element={<Confirmation />} />
+      <Route path="/notification" element={<Notification />} />
+    </Routes>
   </div>
-)
+);
 
-export default Operation
+export default Operation;
